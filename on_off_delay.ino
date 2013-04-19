6a736317c692254fd6489dc0b5b510c2edb6c12c
Travis <traviswu@gmail.com>
2013-04-19 19:18:51 -0300

    more functions added

--- on_off_delay.ino
+++ on_off_delay.ino
@@ -7,12 +7,14 @@ trying this again
 int redLed = 13;
 int greenLed = 12;
 int switchPin = 8; 
-bool previousStatus;
+bool ledOn; 
+bool prevButtonStatus;
+
 unsigned long timer;
 
 bool deBounce(bool prevStatus)
 {
-  bool curr = digitalRead(switchPin);
+	bool curr = digitalRead(switchPin);
 	if (curr != prevStatus){
 		delay(5);
 		curr = digitalRead(switchPin);
@@ -20,29 +22,61 @@ bool deBounce(bool prevStatus)
 	return curr;
 }
 
+void dimmer(int ledPin, bool on)
+{
+	int dimLevel;
+	for(int i = 20; i < 256; i++)
+	{
+		if (on){
+			dimLevel = i;
+		}
+		else {
+			dimLevel = 	256 - i;
+		}
+		analogWrite(ledPin, dimLevel);
+		delay(50);
+	}
+}
+
+
+void autoOff(int ledPin, int seconds)
+{
+	unsigned long currTime = millis();
+	if (currTime - timer > seconds * 1000 && ledOn == HIGH)
+	{
+		ledOn = !ledOn;
+		// digitalWrite(ledPin, ledOn);
+		dimmer(ledPin, ledOn);
+	}
+}
 
 void void setup() 
 {
 	pinMode(redLed, OUTPUT);
 	pinMode(greenLed, OUTPUT);
 	pinMode(switchPin, INPUT);
-	previousStatus = LOW;
+	prevButtonStatus = LOW;
+	ledOn = LOW;
 }
 
 void void loop() 
 {
-	bool currStatus = deBounce(previousStatus);
-	if (currStatus == HIGH){
-		previousStatus = !previousStatus;
-		digitalWrite(redLed, previousStatus);
-		digitalWrite(greenLed, previousStatus);
-		if (previousStatus) timer = millis(); 
+	bool currButtonStatus = deBounce(prevButtonStatus);
+	if (currButtonStatus == HIGH && prevButtonStatus == LOW)
+	{
+		ledOn = !ledOn; 
+		if (ledOn == HIGH) timer = millis(); 
 	}
+	
+	prevButtonStatus = currButtonStatus;
 
-	unsigned long currTime = millis(); 
-	if (currTime - timer > 5000 && previousStatus == HIGH){
-		previousStatus = !previousStatus;
-		digitalWrite(redLed, previousStatus);
-		digitalWrite(greenLed, previousStatus);
-	} 
+	dimmer(redLed, HIGH);
+	dimmer(greenLed, HIGH);
+
+	// digitalWrite(redLed, ledOn);
+	// digitalWrite(greenLed, ledOn);
+
+	autoOff(redLed);
+	autoOff(greenLed);
 }
+
